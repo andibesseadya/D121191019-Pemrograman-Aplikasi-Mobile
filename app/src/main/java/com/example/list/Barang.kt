0c2e27cf2712082cebe0aac646956c7b963ad693
AndiBesseAdya <adyaadias87@gmail.com>
2021-06-11 20:20:40 +0800

    Create Barang Class

---
+++ app/src/main/java/com/example/list/Barang.kt
@@ -0,0 +1,15 @@
+package com.example.list
+
+class Barang {
+    var id: Int? = null
+    var name: String? = null
+    var jenis: String? = null
+    var harga: String? = null
+
+    constructor(id: Int, name: String, jenis: String, harga:String){
+        this.id = id
+        this.name = name
+        this.jenis = jenis
+        this.harga = harga
+    }
+}
